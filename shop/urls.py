92b136356c92a98511a8da243586dfb6c1ae7d8c
1208kelly <1208kelly@gmail.com>
2020-08-25 20:03:09 +0000

    fixing error with shopping cart url

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.shop, name="shop"),
-    path('shoppingcart/', views.shopping_cart, name="shopping cart"),
+    path('shoppingcart/', views.shopping_cart, name="shopping_cart"),
     path('checkout/', views.checkout, name="checkout")
 ]
