a297f910ba5e16915324fef2da7633b8669f33a1
1208kelly <1208kelly@gmail.com>
2020-08-27 20:27:37 +0000

    Added media images

--- coolest_super_power/settings.py
+++ coolest_super_power/settings.py
@@ -149,3 +149,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
