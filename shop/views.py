a6664ac926eaef4867ee6c78694fc34a0afeb564
1208kelly <1208kelly@gmail.com>
2020-08-28 17:15:49 +0000

    Updated shopping cart to reflect cart total price

--- shop/views.py
+++ shop/views.py
@@ -3,20 +3,23 @@ from .models import *
 
 # Create your views here.
 
+
 def shop(request):
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products': products}
     return render(request, 'shop/shop.html', context)
+
 
 def shopping_cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False) # get or create from stack overflow and djangoproject.com
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)  # get or create from stack overflow and djangoproject.com
         items = order.orderitem_set.all()
     else:
         items = []
-	context = {'items':items}
-	return render(request, 'shop/shopping_cart.html', context)
+    context = {'items': items, 'order': order}
+    return render(request, 'shop/shopping_cart.html', context)
+
 
 def checkout(request):
 	context = {}
