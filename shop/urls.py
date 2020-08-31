9c75ef49e0030d64b4a468241116fa4e9e88b75e
1208kelly <1208kelly@gmail.com>
2020-08-31 19:24:39 +0000

    Updated error when trying to add item to shopping cart then checkout

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.shop, name="shop"),
     path('shoppingcart/', views.shopping_cart, name="shopping_cart"),
     path('checkout/', views.checkout, name="checkout"),
-    path('update_item/', views.updateItem, name="update_item")
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
 ]
