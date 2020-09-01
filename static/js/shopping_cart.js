9c82d6e2ca3b598704638b18a76ed9c0c483ebe5
1208kelly <1208kelly@gmail.com>
2020-09-01 19:53:16 +0000

    Fixing error with adding item to shopping cart

--- static/js/shopping_cart.js
+++ static/js/shopping_cart.js
@@ -1,39 +1,39 @@
-var updateButtons = document.getElementsByClassName('update-shopping-cart')
+var updateButtons = document.getElementsByClassName('update-shopping-cart');
 
 for (i = 0; i < updateButtons.length; i++) {
     updateButtons[i].addEventListener('click', function(){
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId, 'Action:', action)
-        console.log('User:', user)
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log('productId:', productId, 'Action:', action);
+        console.log('User:', user);
         if (user == 'AnonymousUser'){
-            console.log('User not signed in')
+            console.log('User not signed in');
         }else {
-            updateUserOrder(productId, action)
+            updateUserOrder(productId, action);
         }
-    })
+    });
 }
 
 function updateUserOrder(productId, action){
-    console.log('Sign in successfull, granting access...')
-    var url = '/update_item/'
-    console.log('URL:', url)
+    console.log('Sign in successfull, granting access...');
+    var url = '/update_item/';
+    console.log('URL:', url);
 
     fetch(url, {
         method:'POST',
         headers:{
-            'Content-Type':'application/json',
+            'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'X-CSRFToken':csrftoken,
+            'X-CSRFToken': csrftoken,
         },
         body:JSON.stringify({'productId': productId, 'action': action})
     })
 
     .then((response) => {
-        return response.json();
+        return response.json()
     })
 
-    .then((_data) => {
+    .then((data) => {
         location.reload()
-    });
+    })
 }
