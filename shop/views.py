45f761ac2bab6cd1443124939d5f6af00aba1977
1208kelly <1208kelly@gmail.com>
2020-09-02 17:02:13 +0000

    Updating shopping cart views

--- shop/views.py
+++ shop/views.py
@@ -25,10 +25,15 @@ def shop(request):
 
 def shopping_cart(request):
 
-    data = cartData(request)
-    cartItems = data['cartItems']
-    order = data['order']
-    items = data['items']
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'shop/shopping_cart.html', context)
