8d5805b8dc6729b98739492717b537c03ae3e42c
1208kelly <1208kelly@gmail.com>
2020-10-01 20:30:13 +0000

    Attempting to fix ongoing issue with deployment

--- coolest_super_power/wsgi.py
+++ coolest_super_power/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for my_online_store project.
+WSGI config for coolest_super_power project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_online_store.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coolest_super_power.settings')
 
 application = get_wsgi_application()
