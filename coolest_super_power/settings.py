389177765dd938932d43960344afbbdd105debd5
1208kelly <1208kelly@gmail.com>
2020-10-29 19:24:59 +0000

    Rebuilding database and products

--- coolest_super_power/settings.py
+++ coolest_super_power/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['e-commerce-full-stack-project.herokuapp.com', 'localhost']
 
