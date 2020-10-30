8a61f92c52702670c17982034c6fa8a6c7d57260
1208kelly <1208kelly@gmail.com>
2020-10-30 18:14:23 +0000

    set up environment variables

--- coolest_super_power/settings.py
+++ coolest_super_power/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['e-commerce-full-stack-project.herokuapp.com', 'localhost']
 
